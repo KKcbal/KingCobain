70e4ed97bc4ba040580fa418da3f075a0d55d9b5
KKcbal <tabbed.out3321@gmail.com>
2024-04-05 11:24:01 -0700

    convert

---
+++ java/Blackjack/Cards.java
@@ -0,0 +1,22 @@
+package java.Blackjack;
+
+public class Cards {
+    public String number;
+    public String symbol;
+    public String color;
+
+    public Cards(String number, String symbol) {
+        this.number = number;
+        this.symbol = symbol;
+    }
+
+    public void createDeck() {
+        for (int i = 1; i <= 13; i++) {
+            if (i == 1) {}
+        }
+    }
+
+    public static void main(String[] args) {
+        //
+    }
+}
---
+++ java/Blackjack/Count.java
@@ -0,0 +1,12 @@
+package java.Blackjack;
+
+public class Count {
+    public int cardCount;
+
+    public Count(int cardCount) {
+        this.cardCount = cardCount;
+    }
+    public static void main(String[] args) {
+        Count cardNum = new Count(1);
+    }
+}
